fbdcb3df00ad9568d054485243fc1eb8a12b7caf
kristban <kristinbannasch@hotmail.com>
2025-02-22 16:55:14 +0000

    improved the form in html, also improved the JS

--- src/index.js
+++ src/index.js
@@ -14,9 +14,12 @@ function generatePoem(event) {
   let keyWordEntry = document.querySelector("#keywords");
   let key = "7a9df9a4f940btc116d43db796o3aa67";
   let context =
-    "You are a German poet who is famous for the his short poesm. Use the following instruction: Create a 4 line poems in basic HTML, only show the 4 lines without heading or other instructions. do not mention html";
+    "You are a German poet who is famous for the his short poesm. Use the following instruction: Create a 4 line poems in basic HTML, only show the 4 lines with <br /> between each line. Do not use heading or other instructions. do not mention html";
   let prompt = `Generate a German poem about ${keyWordEntry.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+let resultElement = document.querySelector("#result")
+resultElement.classList.remove("hidden")
 
   axios.get(apiURL).then(showPoem);
 
