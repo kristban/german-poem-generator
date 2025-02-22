1c47a87f1ef796522ea3f9415f28d77496b07318
kristban <kristinbannasch@hotmail.com>
2025-02-22 16:22:07 +0000

    added the AI API and implemented search in JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem (event) {
-    event.preventDefault();
+function showPoem(response) {
+new Typewriter("#result", {
+  strings: response.data.answer,
+  autoStart: true,
+  cursor: null,
+  delay: 1,
+});
 
-    new Typewriter("#result", {
-      strings: "Hello World",
-      autoStart: true,
-      cursor: null,
-      delay: 1,
-    });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let keyWordEntry = document.querySelector("#keywords");
+  let key = "7a9df9a4f940btc116d43db796o3aa67";
+  let context =
+    "You are a German poet who is famous for the his short poesm. Use the following instruction: Create a 4 line poems in basic HTML, only show the 4 lines without heading or other instructions. do not mention html";
+  let prompt = `Generate a German poem about ${keyWordEntry.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  axios.get(apiURL).then(showPoem);
+
+  
 }
 
 let poemElement = document.querySelector("#poem-gernerator-form");
